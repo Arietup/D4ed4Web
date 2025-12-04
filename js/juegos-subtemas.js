2a824b447321757fe99eee02abff1cf10d7fdbcb
Dario <dario.estupi@hotmail.com>
2025-12-04 16:43:39 -0500

    contenido minecraft

--- js/juegos-subtemas.js
+++ js/juegos-subtemas.js
@@ -3,13 +3,49 @@ window.juegosSubtemas = {
     minecraft: `
         <h3>Minecraft</h3>
         <p>
-            <strong>Minecraft</strong> es mucho más que un simple juego de bloques. Es un espacio de creatividad infinita, donde cada mundo es único y cada aventura depende de ti. 
+            Para fines narrativos, por favor te invito a escuchar el audio adjunto.
             <br><br>
-            Lo que más me marcó de Minecraft es la sensación de libertad absoluta, la posibilidad de construir, explorar y perderme en paisajes generados al azar. 
+            <media-theme-tailwind-audio
+                style="--media-primary-color: #811818; --media-secondary-color: #000000; --media-accent-color: #930101; width:100%;max-width:400px;display:block;margin:0 auto 1.2em auto;">
+                <audio
+                    slot="media"
+                    src="audio/minecraft.mp3"
+                    playsinline
+                    crossorigin="anonymous"
+                ></audio>
+            </media-theme-tailwind-audio>
+            <strong>Minecraft</strong> como concepto es complicado. Definirlo es caer en un sin fin de categorías e ideas. En este apartado será
+            un tanto reflexivo y entorno a mi experiencia propia.
             <br><br>
-            La música ambiental, los atardeceres cúbicos y la tranquilidad de una cueva iluminada por antorchas son recuerdos que siempre llevo conmigo.
+            Llegar un día a las 12 del mediodía y probar por primera vez en mi vida la demo de Playstation 3 fue un cambio de paradigma total respecto
+            a cualquier juego que haya podido probar hasta aquel momento. Con aproximadamente 8 años el concepto, la música y la éstetica del propio juego
+            era algo que transmitia demasiadas emociones y sencaciones que ningún juego habia logrado despertar antes.
+            <br><br>
+            Posiblemente haya jugado a la versión 1.3 o 1.4 del juego, versiones muy tempranas del mismo y con los visuales de un juego que no había sido
+            destruido por parte de microsoft, sino que por el contrario, le seguía perteneciendo al creador original del juego.
+            <br><br>
+            Desde sentirte totalmente solo en un mundo que no tiene fin, desconocer totalmente de las capacidades totales del juego, la imposibilidad
+            de realizar acciones y la invitación al jugador de experimentar como sea posible.
+            <br><br>
+            ¿Cómo puede ser posible que la demo de un juego le haya podido dedicar tantas horas sin parar?
+            <br>
+            <img src="img/sources/minecraft-creeper-boat.jpg" alt="Boat creeper" style="max-width:100%; margin-top:1em; display:block; margin-left:auto; margin-right:auto;">
+            <br>
+            Luego de esta grata experiencia comencé a adentrarme cada vez más en el juego, incluso cayendo en las 
+            tantas copias para android, juegos flash y todo lo parecido al juego, hasta que finalmente en el año 2014 comencé a jugarlo debidamente.
+            <br><br>
+            Este juego tiene un espacio bastante importante dentro de mi vida, ya que sin duda, aunque sin cifras especificas, es el juego
+            al que más horas le he dedicado más horas con diferencia a cualquier otro juego, siendo al menos alrededor de 800 y 1200 horas en total.
+            <br>
+            <img src="img/sources/minecraft-cave.jpg" alt="cave" style="max-width:100%; margin-top:1em; display:block; margin-left:auto; margin-right:auto;">
+            <br>
+            A día de hoy, pese a que no le sigo dedicando tiempo como antes, no lo he dejado de considerar una de las mejores 
+            experiencias dentro dea algún juego. Diferenciandose totalmente de cuaqluier juego actualmente, marcando incluso un antes
+            y un después en mi forma de apreciar el tiempo de ocio.
+            <br>
+            <img src="img/sources/minecraft-sunset.jpg" alt="cave" style="max-width:100%; margin-top:1em; display:block; margin-left:auto; margin-right:auto;">
         </p>
-    `,
+        `,
     undertale: `
         <h3>Undertale</h3>
         <p>
@@ -18,7 +54,9 @@ window.juegosSubtemas = {
             Lo que más admiro es cómo logra transmitir emociones profundas con recursos simples, y cómo juega con las expectativas del jugador. 
             <br><br>
             La música, el humor y los momentos de reflexión hacen de Undertale un juego inolvidable.
+            
             <img src="img/sources/floweydialogue.gif" alt="Undertale" style="max-width:100%;margin-top:1em;">
+
             Desde el primer momento en el que el juego inicia comienzan las sorpresas, no me pienso alargar de más
             pero sin duda, el haberle dedicado tantas horas a este juego ha sido una de las mejores decisiones que he tomado.
             <br> <br>
@@ -26,7 +64,9 @@ window.juegosSubtemas = {
             especialmente interesante que toda la música haya sido compuesta por el mismo creador del juego y antes de empezar
             la misma creación del juego. <br>
             Por ello es el juego el que está ajustado a la música y no al revés. <br>
+            
             <img src="img/sources/sansdialogue.jpg" alt="Sans dialogo" style="max-width:100%;margin-top:1em;">
+            
             <br> <br>
             Te dejo a continuación algunos de los que considero los mejores temas del juego: <br>
            <ul>
@@ -35,7 +75,9 @@ window.juegosSubtemas = {
                 <li><a href="https://www.youtube.com/watch?v=I-IiC2YhB9o" target="_blank">His Theme</a></li>
                 <li><a href="https://www.youtube.com/watch?v=kPSXAVs2yqg" target="_blank">Battle Against a True Hero</a></li>
             </ul>
+            
             <img src="img/sources/sigues-siendo-tu.jpg" alt="Sans dialogo" style="max-width:100%;margin-top:1em;">
+            
             Es brutal como al final del juego te terminas dando cuenta que sigues siendo la misma persona en el fondo.
             <br><br>
             Los tres finales son magistrales y todos se conectan entre si y si tu eres de las personas
